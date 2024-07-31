dd76cf06944045fe55a12dab0272ea15f22e16a5
AXiX-official <2879710747@qq.com>
2024-07-31 15:34:13 +0800

    修改AssetBundle的读取和压缩函数为异步

--- app/src/main/java/top/laoxin/modmanager/tools/AssetBundleJNI.kt
+++ app/src/main/java/top/laoxin/modmanager/tools/AssetBundleJNI.kt
@@ -1,5 +1,8 @@
 package top.laoxin.modmanager.tools
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 /**
  * JNI interface for interacting with native AssetBundle library.
  */
@@ -14,30 +17,11 @@ object AssetBundleJNI {
     const val AB_COMPRESSION_LZ4: Char = 2.toChar()
     const val AB_COMPRESSION_LZ4HC: Char = 3.toChar()
 
-    /**
-     * Loads an AssetBundle from byte array data.
-     *
-     * @param data The byte array containing the AssetBundle data.
-     * @return A pointer to the native AssetBundle object.
-     * @throws RuntimeException if there is an error during loading.
-     */
     @Throws(RuntimeException::class)
-    external fun loadFromBytes(data: ByteArray?): Long
+    private external fun loadFromBytes(data: ByteArray?): Long
 
-    /**
-     * Compresses an AssetBundle to a byte array.
-     *
-     * @param bundlePtr Pointer to the native AssetBundle object.
-     * @param compressionType The compression type to use, which can be one of the following:
-     * - [AssetBundleJNI.AB_COMPRESSION_NONE]
-     * - [AssetBundleJNI.AB_COMPRESSION_LZMA]
-     * - [AssetBundleJNI.AB_COMPRESSION_LZ4]
-     * - [AssetBundleJNI.AB_COMPRESSION_LZ4HC]
-     * @return A byte array containing the compressed AssetBundle data.
-     * @throws RuntimeException if there is an error during compression.
-     */
     @Throws(RuntimeException::class)
-    external fun compressToBytes(bundlePtr: Long, compressionType: Char): ByteArray?
+    private external fun compressToBytes(bundlePtr: Long, compressionType: Char): ByteArray?
 
     /**
      * Gets the compression type of the AssetBundle.
@@ -52,4 +36,31 @@ object AssetBundleJNI {
      */
     @Throws(RuntimeException::class)
     external fun getCompressionType(bundlePtr: Long): Char
+
+    /**
+     * Asynchronously loads an AssetBundle from byte array data.
+     *
+     * @param data The byte array containing the AssetBundle data.
+     * @return A pointer to the native AssetBundle object.
+     * @throws RuntimeException if there is an error during loading.
+     */
+    suspend fun loadFromBytesAsync(data: ByteArray?): Long = withContext(Dispatchers.IO) {
+        loadFromBytes(data)
+    }
+
+    /**
+     * Asynchronously compresses an AssetBundle to a byte array.
+     *
+     * @param bundlePtr Pointer to the native AssetBundle object.
+     * @param compressionType The compression type to use, which can be one of the following:
+     * - [AssetBundleJNI.AB_COMPRESSION_NONE]
+     * - [AssetBundleJNI.AB_COMPRESSION_LZMA]
+     * - [AssetBundleJNI.AB_COMPRESSION_LZ4]
+     * - [AssetBundleJNI.AB_COMPRESSION_LZ4HC]
+     * @return A byte array containing the compressed AssetBundle data.
+     * @throws RuntimeException if there is an error during compression.
+     */
+    suspend fun compressToBytesAsync(bundlePtr: Long, compressionType: Char): ByteArray? = withContext(Dispatchers.IO) {
+        compressToBytes(bundlePtr, compressionType)
+    }
 }
