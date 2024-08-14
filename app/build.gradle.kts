12c88fbb027b541823cbd4f0cc9fde609d1b2c59
lings <90336521+lings03@users.noreply.github.com>
2024-08-14 21:31:17 +0800

    finish ci

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,8 +78,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
