4f5ca1f4e3d0780fb737a614d2c9e3b571d70098
角砂糖 <90336521+lings03@users.noreply.github.com>
2024-11-09 17:29:16 +0800

    fix navi background color
    (cherry picked from commit 069ef24c4919b2627805cde9a7d1094bb867a330)

--- app/src/main/java/top/lings/start/StartActivity.kt
+++ app/src/main/java/top/lings/start/StartActivity.kt
@@ -8,11 +8,17 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.core.view.WindowCompat
 
 class StartActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 设置全屏模式，使内容可以扩展到状态栏和导航栏区域
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        // 通过背景色使状态栏和导航栏透明
+        window.setBackgroundDrawableResource(android.R.color.transparent)
 
         // 启用 Edge-to-Edge 模式
         enableEdgeToEdge()
