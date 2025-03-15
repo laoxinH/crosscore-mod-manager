cbb7e7cfffa10a6596541d2b4bb8d0b35e536a98
laoxinH <thixin@qq.com>
2025-03-16 00:42:24 +0800

    Release 3.5.4
    修改readme为Markdown格式(可自定义任何内容,详细参考Markdown语法规范)
    readme文件命名为readme.txt或readme.md
    添加明日方舟国际服支持(参考实验室指南自行编辑配置文件)
    
    注意!!
    1.启动尘白禁区需要在后台运行的权限
    2.国产系统需要允许在后台允许或者将电池优化(省电策略)设置为无限制
    
    Tips:
    1.MOD列表可以长按，可以长按，可以长按;
    2.长按后有全选和反选选项，可以一件开启或关闭;
    3.分类视图下如果有部分单压缩包内多MOD但点入路径却提示无内容为已知问题，暂未修复，请尽可能使用7z，如使用Zip格式请不要使用系统自带压缩工具;
    4.如果要写Readme文件，请严格按照格式，实验室做了简单的错误处理，但不保证不会出现问题;
    5.所有游戏的MOD扫描以及开关均需要在下载好所有游戏资源后才能正常使用

--- app/src/main/kotlin/top/laoxin/modmanager/domain/usercase/mod/ReadModReadmeFileUserCase.kt
+++ app/src/main/kotlin/top/laoxin/modmanager/domain/usercase/mod/ReadModReadmeFileUserCase.kt
@@ -36,47 +36,47 @@ class ReadModReadmeFileUserCase @Inject constructor() {
         }
     }
     // 读取readme文件
-    fun invoke1(unZipPath: String, modBean: ModBean): ModBean {
-        // 判断是否存在readme文件
-        val infoMap = mutableMapOf<String, String>()
-        if (modBean.readmePath != null) {
-            val readmeFile = File(unZipPath + modBean.readmePath)
-            if (readmeFile.exists()) {
-                val reader = readmeFile.bufferedReader()
-                val lines = reader.readLines()
-                for (line in lines) {
-                    val parts = line.split("：")
-                    if (parts.size == 2) {
-                        val key = parts[0].trim()
-                        val value = parts[1].trim()
-                        infoMap[key] = value
-                    }
-                }
-            }
-        } else if (modBean.fileReadmePath != null) {
-            val readmeFile = File(unZipPath + modBean.fileReadmePath)
-            if (readmeFile.exists()) {
-                val reader = readmeFile.bufferedReader()
-                val lines = reader.readLines()
-                for (line in lines) {
-                    val parts = line.split("：")
-                    if (parts.size == 2) {
-                        val key = parts[0].trim()
-                        val value = parts[1].trim()
-                        infoMap[key] = value
-                    }
-                }
-            }
-        }
-        return if (infoMap.isNotEmpty()) {
-            modBean.copy(
-                name = infoMap["名称"],
-                description = infoMap["描述"],
-                version = infoMap["版本"],
-                author = infoMap["作者"]
-            )
-        } else {
-            modBean
-        }
-    }
+//    fun invoke1(unZipPath: String, modBean: ModBean): ModBean {
+//        // 判断是否存在readme文件
+//        val infoMap = mutableMapOf<String, String>()
+//        if (modBean.readmePath != null) {
+//            val readmeFile = File(unZipPath + modBean.readmePath)
+//            if (readmeFile.exists()) {
+//                val reader = readmeFile.bufferedReader()
+//                val lines = reader.readLines()
+//                for (line in lines) {
+//                    val parts = line.split("：")
+//                    if (parts.size == 2) {
+//                        val key = parts[0].trim()
+//                        val value = parts[1].trim()
+//                        infoMap[key] = value
+//                    }
+//                }
+//            }
+//        } else if (modBean.fileReadmePath != null) {
+//            val readmeFile = File(unZipPath + modBean.fileReadmePath)
+//            if (readmeFile.exists()) {
+//                val reader = readmeFile.bufferedReader()
+//                val lines = reader.readLines()
+//                for (line in lines) {
+//                    val parts = line.split("：")
+//                    if (parts.size == 2) {
+//                        val key = parts[0].trim()
+//                        val value = parts[1].trim()
+//                        infoMap[key] = value
+//                    }
+//                }
+//            }
+//        }
+//        return if (infoMap.isNotEmpty()) {
+//            modBean.copy(
+//                name = infoMap["名称"],
+//                description = infoMap["描述"],
+//                version = infoMap["版本"],
+//                author = infoMap["作者"]
+//            )
+//        } else {
+//            modBean
+//        }
+//    }
 }
